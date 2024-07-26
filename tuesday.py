55cf6ba3da92ccde384b4d9b41dc68cb4bad5343
hasithsen <sen.hasith@gmail.com>
2024-07-26 09:49:49 +0530

    style: Update content formatting

--- tuesday.py
+++ tuesday.py
@@ -8,4 +8,4 @@ app = Flask(__name__)
 @app.route("/")
 def main():
     is_it_tuesday = "yes!" if datetime.datetime.now().strftime("%a") == "Tue" else "no" 
-    return f"Is it Tuesday? <b>{ is_it_tuesday }</b><hr /><code>{ datetime.datetime.now().isoformat() }</code>"
+    return f"<h1>Is it Tuesday? <b>{ is_it_tuesday }</b></h1><hr /><code>{ datetime.datetime.now().isoformat() }</code>"
